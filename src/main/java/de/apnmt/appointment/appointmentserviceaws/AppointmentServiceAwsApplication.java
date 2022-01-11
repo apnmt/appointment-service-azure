18dc8e8fd085957c4e0f40cd835b4d2f69c85ffd
TWI <audi-connect@msg.group>
2022-01-11 10:08:38 +0100

    initial commit

---
+++ src/main/java/de/apnmt/appointment/appointmentserviceaws/AppointmentServiceAwsApplication.java
@@ -0,0 +1,13 @@
+package de.apnmt.appointment.appointmentserviceaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentServiceAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentServiceAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/apnmt/appointment/appointmentserviceaws/AppointmentServiceAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.apnmt.appointment.appointmentserviceaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
