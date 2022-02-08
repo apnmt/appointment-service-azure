132085a8478db9800270a9a90e8dc38f393cb0ff
tobi5775 <tobias.wittmann96@web.de>
2022-02-08 15:46:17 +0100

    add messaging via Azure ServiceBus

--- src/main/java/de/apnmt/appointment/messaging/sender/AppointmentEventSender.java
+++ src/main/java/de/apnmt/appointment/messaging/sender/AppointmentEventSender.java
@@ -1,10 +1,11 @@
-package de.apnmt.appointment.sender;
+package de.apnmt.appointment.messaging.sender;
 
 import de.apnmt.common.event.ApnmtEvent;
 import de.apnmt.common.event.value.AppointmentEventDTO;
 import de.apnmt.common.sender.ApnmtEventSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,8 +13,15 @@ public class AppointmentEventSender implements ApnmtEventSender<AppointmentEvent
 
     private static final Logger log = LoggerFactory.getLogger(AppointmentEventSender.class);
 
+    private JmsTemplate jmsTemplate;
+
+    public AppointmentEventSender(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
     @Override
     public void send(String topic, ApnmtEvent<AppointmentEventDTO> event) {
         log.info("Send event {} to Service Bus topic {}", event, topic);
+        jmsTemplate.convertAndSend(topic, event);
     }
 }
--- src/main/java/de/apnmt/appointment/messaging/sender/ServiceEventSender.java
+++ src/main/java/de/apnmt/appointment/messaging/sender/ServiceEventSender.java
@@ -1,10 +1,11 @@
-package de.apnmt.appointment.sender;
+package de.apnmt.appointment.messaging.sender;
 
 import de.apnmt.common.event.ApnmtEvent;
 import de.apnmt.common.event.value.ServiceEventDTO;
 import de.apnmt.common.sender.ApnmtEventSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,8 +13,15 @@ public class ServiceEventSender implements ApnmtEventSender<ServiceEventDTO> {
 
     private static final Logger log = LoggerFactory.getLogger(ServiceEventSender.class);
 
+    private JmsTemplate jmsTemplate;
+
+    public ServiceEventSender(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
     @Override
     public void send(String topic, ApnmtEvent<ServiceEventDTO> event) {
         log.info("Send event {} to Service Bus topic {}", event, topic);
+        jmsTemplate.convertAndSend(topic, event);
     }
 }
